a1a407b8a720d84b1b78639426167a4b6b241e2d
Mogyorósi Bálint <blnt.mogyorosi@gmail.com>
2020-08-08 21:50:56 +0200

    Added code for production rendering

--- server.js
+++ server.js
@@ -25,6 +25,15 @@ require('./config/passport')(passport);
 // Apply custom routes
 app.use('/api/users', usersRoutes);
 
+// Server static assets if in production
+if (process.env.NODE_ENV === 'production') {
+    // Set static folder
+    app.use(express.static('client/build'))
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    })
+}
+
 // Start the application
 const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
